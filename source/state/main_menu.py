7af2ca08f3c10b6160866f8b7c80a39c8d4c114f
THE-ONE <lwyx12620@163.com>
2026-03-18 15:11:22 +0800

    text data

--- source/state/main_menu.py
+++ source/state/main_menu.py
@@ -1,12 +1,14 @@
 import pygame
 from source import setup,tools
 from source import constants as C
+from source.components import info
 
 class MainMenu:
     def __init__(self):
         self.setup_background()
         self.setup_player()
         self.setup_cursor()
+        self.info = info.Info('main_menu')
 
     def setup_background(self):
         self.background = setup.GRAPHICS['level_1']
@@ -27,6 +29,9 @@ class MainMenu:
         surface.blit(self.caption,(170,100))
         surface.blit(self.player_image,(110,490))
         surface.blit(self.cursor,(220,360))
+
+        self.info.update()
+        self.info.draw(surface)
 
 
 
