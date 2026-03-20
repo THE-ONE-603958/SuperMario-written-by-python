8ee381e1c595770cae97a461ae8d60010f36735e
THE-ONE <lwyx12620@163.com>
2026-03-20 10:15:49 +0800

    Mario enter

---
+++ source/components/player.py
@@ -0,0 +1,102 @@
+import pygame
+from source import setup,tools
+from source import constants as C
+import json
+import os
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,name):
+        pygame.sprite.Sprite.__init__(self)
+        self.name = name
+        self.load_data()
+        self.setup_player_states()
+        self.setup_velocities()
+        self.setup_timers()
+        self.load_images()
+
+    def load_data(self):
+        file_name = self.name + '.json'
+        file_path = os.path.join('source/data/player', file_name)
+        with open(file_path) as f:
+            self.player_data = json.load(f)
+
+    def setup_player_states(self):
+        self.face_right = True
+        self.dead = False
+        self.big = False
+
+    def setup_velocities(self):
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def setup_timers(self):
+        self.walking_timer = 0
+        self.transition_timer = 0
+
+    def load_images(self):
+        sheet = setup.GRAPHICS['mario_bros']
+        frame_rects = self.player_data['image_frames']
+        self.right_small_normal_frames = []
+        self.right_big_normal_frames = []
+        self.right_big_fire_frames = []
+        self.left_small_normal_frames = []
+        self.left_big_normal_frames = []
+        self.left_big_fire_frames = []
+
+        self.small_normal_frames = [self.right_small_normal_frames, self.left_small_normal_frames]
+        self.big_normal_frames = [self.right_big_normal_frames, self.left_big_normal_frames]
+        self.big_fire_frames = [self.right_big_fire_frames, self.left_big_fire_frames]
+
+        self.all_frames = [
+            self.right_small_normal_frames,
+            self.right_big_normal_frames,
+            self.right_big_fire_frames,
+            self.left_small_normal_frames,
+            self.left_big_normal_frames,
+            self.left_big_fire_frames
+        ]
+
+        self.right_frames = self.right_small_normal_frames
+        self.left_frames = self.left_small_normal_frames
+
+        for group, group_frame_rects in frame_rects.items(): #返回字典中的所有键值对,每个键值对是一个元组
+            for frame_rect in group_frame_rects:
+                right_image = tools.get_image(sheet, frame_rect['x'], frame_rect['y'],
+                                              frame_rect['width'], frame_rect['height'],(0,0,0),C.PLAYER_MULTI)
+                left_image = pygame.transform.flip(right_image, True, False)
+                if group == 'right_small_normal':
+                    self.right_small_normal_frames.append(right_image)
+                    self.left_small_normal_frames.append(left_image)
+                if group == 'right_big_normal':
+                    self.right_big_normal_frames.append(right_image)
+                    self.left_big_normal_frames.append(left_image)
+                if group == 'right_big_fire':
+                    self.right_big_fire_frames.append(right_image)
+                    self.left_big_fire_frames.append(left_image)
+
+            self.frame_index = 0
+            self.frames = self.right_frames
+            self.image = self.frames[self.frame_index]
+            self.rect = self.image.get_rect()
+
+    def update(self,keys):
+        self.current_time = pygame.time.get_ticks()
+
+        if keys[pygame.K_RIGHT]:
+            self.x_vel = 5
+            self.y_vel = 0
+            self.frames = self.right_frames
+        if keys[pygame.K_LEFT]:
+            self.x_vel = -5
+            self.y_vel = 0
+            self.frames = self.left_frames
+
+        if self.current_time - self.walking_timer > 100:
+            self.walking_timer = self.current_time
+            self.frame_index += 1
+            self.frame_index %= 4
+        self.image = self.frames[self.frame_index]
+
+
+
+
--- source/state/level.py
+++ source/state/level.py
@@ -1,6 +1,6 @@
 import pygame
 from source import setup
-from source.components import info
+from source.components import info,player
 from source import constants as C
 
 
@@ -8,8 +8,11 @@ class Level:
     def __init__(self):
         self.finished = False
         self.next_state = None
-        self.setup_background()
         self.info = info.Info('level')
+        self.setup_background()
+        self.player = player.Player('mario')
+        self.setup_player()
+
 
     def setup_background(self):
         self.background = setup.GRAPHICS['level_1']
@@ -17,9 +20,21 @@ class Level:
         self.background = pygame.transform.scale(self.background, (int(self.background_rect.width * C.BG_MULTI),
                                                                    int(self.background_rect.height * C.BG_MULTI)))
 
+    def setup_player(self):
+        self.player.rect.x = 300
+        self.player.rect.y = 300
+
     def update(self,surface,keys):
+        self.player.update(keys)
+        self.update_player_position()
         self.draw(surface)
 
+    def update_player_position(self):
+        self.player.rect.x += self.player.x_vel
+        self.player.rect.y += self.player.y_vel
+
     def draw(self,surface):
-        surface.blit(self.background, self.background_rect)
+        surface.blit(self.background, (0,0))
+        surface.blit(self.player.image, self.player.rect)
         self.info.draw(surface)
+        self.info.update()
